018b66efa6913867a973d96c5060b8820830e310
nikkagarperio <nikka.garperio@pointwest.com.ph>
2017-11-21 19:40:31 +0800

    Added Employee bean

---
+++ Employee.java
@@ -0,0 +1,15 @@
+public class Employee {
+	private int employeeId;
+	
+	public Employee() {
+		this.employeeId = 1;
+	}
+	
+	public void setEmployeeId (int employeeID) {
+		this.employeeId = employeeId;
+	}
+	
+	public int getEmployeeId() {
+		return employeeId;
+	}
+}
